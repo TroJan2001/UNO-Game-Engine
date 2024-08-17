cc938e4e0b570c034710193a08ce52c05cf36ca2
TroJan2001 <99925582+TroJan2001@users.noreply.github.com>
2024-08-17 22:44:47 +0300

    Remove a comment

--- src/core/GameDriver.java
+++ src/core/GameDriver.java
@@ -4,8 +4,7 @@ import factory.GameFactory;
 
 public class GameDriver {
     public static void main(String[] args) {
-        Game game = GameFactory.createGame("Classic");
-        //Game game = GameFactory.createGame("Custom");
+        Game game = GameFactory.createGame("Custom");
         game.play();
 
     }
